ab2c59581c0378216a2d3752ce92750454a5edd9
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2025-03-04 11:43:37 +0700

    1

--- script.test.js
+++ script.test.js
@@ -4,20 +4,33 @@ import { loadData } from './loadData';
 jest.mock('./loadData'); 
 
 test('Проверка calcStatsFromAPI и что loadData вызывается 1 раз', async () => {
-    const mockData = [
-        { country: 'USA' },
-        { country: 'USA' },
-        { country: 'Canada' }
-    ];
+    const mockData = [{
+        breed: 'Turkish Van',
+        country: 'developed in the United Kingdom (founding stock from Turkey)',
+        origin: 'Natural',
+        coat: 'Semi-long',
+        pattern: 'Van'
+    },
+    {
+        breed: 'York Chocolate',
+        country: 'United States (New York)',
+        origin: 'Natural',
+        coat: 'Long',
+        pattern: 'Solid'
+    }
+];
+
+const loadDataMock = jest.spyOn({ loadData }, 'loadData').mockResolvedValue(mockData);
     
     loadData.mockResolvedValue(mockData); 
 
     const result = await calcStatsFromAPI();
-    expect(loadData).toHaveBeenCalledTimes(1);
+    expect(loadDataMock).toHaveBeenCalledTimes(1);
     expect(result).toEqual({
-        USA: 2,
-        Canada: 1
-    }); 
+        'developed in the United Kingdom (founding stock from Turkey)': 1,
+        'United States (New York)': 1
+    });
+    loadDataMock.mockRestore();
 });
 
 
